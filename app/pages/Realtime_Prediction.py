43f2b42dd5c0b54792b9beffe1c12f239252f55a
Awaliyatul Hikmah <imadlz15@gmail.com>
2023-07-24 05:01:50 +0700

    update rtc_configuration

--- app/pages/Realtime_Prediction.py
+++ app/pages/Realtime_Prediction.py
@@ -13,26 +13,35 @@ with st.spinner("Retriving data from Redis"):
 
 st.success("Data retrived successfully")
 
-waitTime = 30 # seconds
+waitTime = 30  # seconds
 setTime = time.time()
-realtimepred = face_rec.RealTimePred() #realtime predict class
+realtimepred = face_rec.RealTimePred()  # realtime predict class
+
 
 def video_frame_callback(frame):
-  global setTime
+    global setTime
 
-  img = frame.to_ndarray(format="bgr24") # 3 dimensional array
+    img = frame.to_ndarray(format="bgr24")  # 3 dimensional array
 
-  pred_img = realtimepred.face_prediction(img, redis_face_db, feature_column="facial_features", thresh=0.5)
-  timenow = time.time()
-  difftime = timenow - setTime
+    pred_img = realtimepred.face_prediction(
+        img, redis_face_db, feature_column="facial_features", thresh=0.5
+    )
+    timenow = time.time()
+    difftime = timenow - setTime
 
-  if difftime >= waitTime:
-    realtimepred.saveLogs_redis()
-    setTime = time.time() # reset the time
+    if difftime >= waitTime:
+        realtimepred.saveLogs_redis()
+        setTime = time.time()  # reset the time
 
-    print('saved to redis')
+        print("saved to redis")
 
-  return av.VideoFrame.from_ndarray(pred_img, format="bgr24")
+    return av.VideoFrame.from_ndarray(pred_img, format="bgr24")
 
 
-webrtc_streamer(key="realtimePrediction", video_frame_callback=video_frame_callback)
+webrtc_streamer(
+    key="realtimePrediction",
+    video_frame_callback=video_frame_callback,
+    rtc_configuration={  # Add this config
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    },
+)
--- app/pages/Registration_Form.py
+++ app/pages/Registration_Form.py
@@ -18,21 +18,28 @@ role = st.selectbox("Select Role", ["Student", "Teacher"])
 
 # 2. collect facial embedding
 def video_callback_func(frame):
-  img = frame.to_ndarray(format="bgr24") # 3 dimensional array
-  reg_img, embedding = registration_form.get_embedding(img)
+    img = frame.to_ndarray(format="bgr24")  # 3 dimensional array
+    reg_img, embedding = registration_form.get_embedding(img)
 
-  if embedding is not None:
-    with open('face_embedding.txt', mode='ab') as f:
-      np.savetxt(f, embedding)
+    if embedding is not None:
+        with open("face_embedding.txt", mode="ab") as f:
+            np.savetxt(f, embedding)
 
-  return av.VideoFrame.from_ndarray(reg_img, format="bgr24")
-
-webrtc_streamer(key="registration", video_frame_callback=video_callback_func)
+    return av.VideoFrame.from_ndarray(reg_img, format="bgr24")
 
 
-if st.button('Submit'):
-  return_val = registration_form.save_data_in_redis_db(name, role)
-  if return_val == True:
-    st.success("Data saved successfully")
-  else:
-    st.error("Something went wrong")
+webrtc_streamer(
+    key="registration",
+    video_frame_callback=video_callback_func,
+    rtc_configuration={  # Add this config
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    },
+)
+
+
+if st.button("Submit"):
+    return_val = registration_form.save_data_in_redis_db(name, role)
+    if return_val == True:
+        st.success("Data saved successfully")
+    else:
+        st.error("Something went wrong")
